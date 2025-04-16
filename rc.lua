05560dd1fd3c39259e06ee3344c578107143d1cd
Joshua <xeon826@gmail.com>
2025-04-15 20:17:39 -0500

    reverse hotkeys

--- rc.lua
+++ rc.lua
@@ -416,10 +416,10 @@ globalkeys = gears.table.join(
 		awful.client.swap.byidx(-1)
 	end, { description = "swap with previous client by index", group = "client" }),
 	awful.key({ modkey, "Control" }, "k", function()
-		awful.screen.focus_relative(1)
+		awful.screen.focus_relative(-1)
 	end, { description = "focus the next screen", group = "screen" }),
 	awful.key({ modkey, "Control" }, "j", function()
-		awful.screen.focus_relative(-1)
+		awful.screen.focus_relative(1)
 	end, { description = "focus the previous screen", group = "screen" }),
 	awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
 	awful.key({ modkey }, "Tab", function()
